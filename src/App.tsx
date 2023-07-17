2f6dd677f7f6fe9649651d42f424b2a8d2c2f451
Carfexz <101093051+Carfexz@users.noreply.github.com>
2023-07-17 14:51:09 +0300

    [ADD] Customization tsconfig.json

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./components/widgets/Footer/Footer";
+import './styles/fonts.css'
+import './test.css'
+
+const App = () => {
+  return (
+    <div className='test'>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/widgets/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = ({ text = 'work' }) => {
+    return (
+        <div>
+            {text}
+        </div>
+    )
+}
+
+export default Footer;
